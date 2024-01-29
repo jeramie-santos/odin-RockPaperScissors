f9446a891203109c8c77d82c9c379467967c9561
Jeramie-Santos <jeramieramsantos79@gmail.com>
2024-01-29 14:28:00 +0800

    Play Round

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function getComputerChoice(){
     if (randomPick == 1){
         botChoice = "Rock";
     } else if (randomPick == 2){
-        botChoice = "Papper";
+        botChoice = "Paper";
     } else if (randomPick == 3){
         botChoice = "Scissors";
     }
@@ -14,10 +14,37 @@ function getComputerChoice(){
 }
 
 function playerSelection(){
-    let playerChoice = prompt("Pick Rock, Papper, Scissors: ");
+    let playerChoice = prompt("Pick (Rock, Paper, Scissors) ");
     return playerChoice;
 }
 
+function playRound(playerSelection,computerSelection){
+    
+    let playerLower = playerSelection.toLowerCase();
+    let compLower = computerSelection.toLowerCase();
 
-playerSelection();
-getComputerChoice();
+    if (playerLower == compLower){
+            return ("Draw! you both pick " + computerSelection);
+    } else if (playerLower == "rock" && compLower == "scissors") {
+            return "You Win! Rock beats " + computerSelection;
+    } else if (playerLower == "paper" && compLower == "rock") {
+            return "You Win! Paper beats " + computerSelection;
+    } else if (playerLower == "scissors" && compLower == "paper") {
+            return "You Win! Scissors beats " + computerSelection;
+    }  else if (playerLower == "rock" && compLower == "paper"){
+            return "You Lose! " + computerSelection + " beats Rocks";
+    }  else if (playerLower == "paper" && compLower == "scissors"){
+            return "You Lose! " + computerSelection + " beats Paper";
+    }  else if (playerLower == "scissors" && compLower == "rock"){
+            return "You Lose! " + computerSelection + " beats Scissors";
+    } else {
+            return "Invalid input!";
+    }
+}
+
+     
+
+const player = playerSelection();
+const computer = getComputerChoice();
+
+console.log(playRound(player, computer));
